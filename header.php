04838568409f441365ab538029e820b0230c4556
Emil Uzelac <emil@uzelac.me>
2014-11-02 22:40:09 -0600

    Closing DIV comment for site-branding
    Call it OCD, but I feel like we need a closing DIV comment for  ```site-branding``` :)

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 			
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', 'marker' ); ?></button>
