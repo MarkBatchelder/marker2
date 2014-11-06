1514600c325e66ddaecc9c6291b503ad0933409c
Konstantin Obenland <obenland@automattic.com>
2014-11-05 17:37:48 -0800

    _s: Improve menu toggle accessibility.
    Makes it a little easier to use the primary navigation with assistive
    devices.
    
    See #545.

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'marker' ); ?></button>
+			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'marker' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
