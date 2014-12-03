d7f358a06bdb3572e922fe84a814edb5e0c70863
Jasper de Groot <mail@ugomobi.com>
2014-12-03 01:18:09 +0100

    Header: Made aria-controls refer to an ID instead of a class

--- header.php
+++ header.php
@@ -39,8 +39,8 @@
 		</div><!-- .site-branding -->
 
 		<nav id="primary-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'marker' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'marker' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
