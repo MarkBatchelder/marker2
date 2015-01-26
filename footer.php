a3f7d7d9f6a657b7fc8e6af20f0a6a3adfef1f3b
Mark Batchelder <code@wpaddiction.com>
2015-01-25 23:02:12 -0800

    Fixed footer for full width websites and added color variables.

--- footer.php
+++ footer.php
@@ -9,14 +9,16 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
+	
+	<div id="footer-primary">
 		<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
 			<div id="tertiary" class="widget-area" role="complementary">
 				<?php dynamic_sidebar( 'sidebar-2' ); ?>
 			</div><!-- #tertiary -->
 		<?php endif; ?>
-		
+	</div><!-- #footer-primary -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'marker' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'marker' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
