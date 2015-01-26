Automattic/_s

Better WordPress Coding standards.
This minor commit kicks off code improvements to _s so that it’s fully in line with WordPress Coding Standards rules.

content.php
@@ -19,7 +19,7 @@
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), 
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>

MarkBatchelder/marker2

content.php
@@ -25,7 +25,7 @@
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'marker' ), 
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'marker' ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>