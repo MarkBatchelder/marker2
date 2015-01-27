Automattic/_s

Update Travis CI build checks. Props @miya0001. See #663.

inc/template-tags.php
@@ -53,7 +53,7 @@ function _s_post_nav() {
 		<div class="nav-links">
 			<?php
 				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span>&nbsp;%title', 'Previous post link', '_s' ) );
-				next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link',     '_s' ) );
+				next_post_link( '<div class="nav-next">%link</div>', _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link', '_s' ) );
 			?>
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->

MarkBatchelder/marker2

inc/template-tags.php
@@ -53,7 +53,7 @@ function marker_post_nav() {
 		<div class="nav-links">
 			<?php
 				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span>&nbsp;%title', 'Previous post link', 'marker' ) );
-				next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link',     'marker' ) );
+				next_post_link( '<div class="nav-next">%link</div>', _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link', 'marker' ) );
 			?>
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->