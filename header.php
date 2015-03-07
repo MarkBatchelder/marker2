a1fb471d058574dd5d94ccfbc051742bcdbc2b7e
Mark Batchelder <code@wpaddiction.com>
2015-03-07 01:55:29 -0800

    Updated navigation names to new CSS names.

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 
 	<header id="masthead" class="site-header" role="banner">
 	
-		<nav id="extra-navigation" class="top-navigation" role="navigation">
+		<nav id="top-navigation" class="secondary-navigation" role="navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'top', 'fallback_cb' => false ) ); ?>
 		</nav><!-- #extra-navigation -->
 	
@@ -38,7 +38,7 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+		<nav id="primary-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'marker' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
