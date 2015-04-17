5858bb62656a1ee28decb058f71ccd818220bf4f
Mark Batchelder <code@wpaddiction.com>
2015-04-17 00:48:26 -0700

    Fixed it so that when home is a page it doesn't display the page title.

--- content-page.php
+++ content-page.php
@@ -7,9 +7,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+    <?php if( is_front_page() ) : ?>
+    	<!-- Don't display page title if front page is static. -->
+    <?php else : ?>
+        <header class="entry-header">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        </header><!-- .entry-header -->
+    <?php endif; ?>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
