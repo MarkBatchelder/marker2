406787118702325869d98403bffe157c0efc72db
Mark Batchelder <code@wpaddiction.com>
2015-04-17 00:51:41 -0700

    Fixed to match new navigation naming.

--- js/navigation.js
+++ js/navigation.js
@@ -6,7 +6,7 @@
 ( function() {
 	var container, button, menu;
 
-	container = document.getElementById( 'site-navigation' );
+	container = document.getElementById( 'primary-navigation' );
 	if ( ! container ) {
 		return;
 	}
