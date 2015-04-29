438a8287c083565dc282d4a4b2c256858a526655
Philip Arthur Moore <philip@pressbuild.com>
2015-04-29 08:42:12 +0700

    Move template parts into their own dedicated folder and update Infinite Scroll to reflect this change. See #642. Props @leopuleo.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -13,7 +13,15 @@
 function marker_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
+		'render'    => '_s_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
-}
+} // end function marker_jetpack_setup
 add_action( 'after_setup_theme', 'marker_jetpack_setup' );
+
+function marker_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+} // end function marker_infinite_scroll_render
