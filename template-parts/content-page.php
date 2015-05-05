ec07cbab27d07d851944f548fa856c88d741a4f0
Fränk Klein <contact@fklein.info>
2015-05-05 12:27:52 +0200

    Escape translations in content-page.php
    See #737.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -19,13 +19,13 @@
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'marker' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'marker' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php edit_post_link( __( 'Edit', 'marker' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( esc_html__( 'Edit', 'marker' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
