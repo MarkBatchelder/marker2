df66b9ea555057dd6534f9321ad814a47c8890fc
Sagar Jadhav <sagar.jadhav@rtcamp.com>
2015-05-29 19:55:58 +0530

    Updated Tagline markup as per WC3 guidelines.

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 			<?php endif; // End header image check. ?>
 			
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 		</div><!-- #logo -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
