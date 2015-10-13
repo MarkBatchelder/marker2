6053c2b27667ab5948d0795237935cededa80fc5
Mark Batchelder <code@wpaddiction.com>
2015-10-12 17:02:05 -0700

    Moved #tertiary into #content for structure consistency.
    Also added #credits to .site-info.

--- footer.php
+++ footer.php
@@ -7,19 +7,19 @@
  * @package Marker
  */
 ?>
-
-	</div><!-- #content -->
 	
-	<div id="footer-primary" class="footer-sidebar">
+	<div id="content-footer" class="footer-sidebar">
 		<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
 			<div id="tertiary" class="footer-widget-area" role="complementary">
 				<?php dynamic_sidebar( 'sidebar-2' ); ?>
 			</div><!-- #tertiary -->
 		<?php endif; ?>
-	</div><!-- #footer-primary -->
+	</div><!-- #content-footer -->
+
+	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		<div id="credits" class="site-info">
 			<div class="copyright">
 				&copy;
 				<?php echo date("Y"); ?>
@@ -29,7 +29,7 @@
 			<div class="theme-info">
 				<?php printf( __( 'Designed & maintained by %s.', 'marker' ), '<a href="http://webfirstdesigns.com/" rel="designer">Web First Designs</a>' ); ?>
 			</div><!-- .theme-info -->
-		</div><!-- .site-info -->
+		</div><!-- #credits -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 </div><!-- #page-container -->
