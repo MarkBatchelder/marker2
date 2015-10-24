Automattic/_s

Secure translations in content.php.
See #737.

template-parts/content.php
@@ -19,7 +19,7 @@
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ),
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array() ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>

MarkBatchelder/marker2

template-parts/content.php
@@ -25,7 +25,7 @@
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'marker' ),
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'marker' ), array( 'span' => array() ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
 		?>