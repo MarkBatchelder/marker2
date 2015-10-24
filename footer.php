4245a08836d04863c3f6743aa72d483f1effd7c9
Mark Batchelder <code@wpaddiction.com>
2015-10-23 21:41:53 -0700

    Escape translations in footer.php
    See #737.

--- footer.php
+++ footer.php
@@ -27,7 +27,7 @@
 				All rights reserved.
 			</div><!-- .copyright -->
 			<div class="theme-info">
-				<?php printf( __( 'Designed & maintained by %s.', 'marker' ), '<a href="http://webfirstdesigns.com/" rel="designer">Web First Designs</a>' ); ?>
+				<?php printf( esc_html__( 'Designed & maintained by %s.', 'marker' ), '<a href="http://webfirstdesigns.com/" rel="designer">Web First Designs</a>' ); ?>
 			</div><!-- .theme-info -->
 		</div><!-- #credits -->
 	</footer><!-- #colophon -->
