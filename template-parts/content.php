Automattic/_s

Secure translations in content.php.
See #737.

template-parts/content.php
@@ -26,7 +26,7 @@
 
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', '_s' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
 				'after'  => '</div>',
 			) );
 		?>

MarkBatchelder/marker2

template-parts/content.php
@@ -32,7 +32,7 @@
 		
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'marker' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'marker' ),
 				'after'  => '</div>',
 			) );
 		?>