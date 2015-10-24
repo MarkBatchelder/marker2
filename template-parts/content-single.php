Automattic/_s

Escape translations in content-single.php
See #737.

template-parts/content-single.php
@@ -17,7 +17,7 @@
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', '_s' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
 				'after'  => '</div>',
 			) );
 		?>

MarkBatchelder/marker2

template-parts/content-single.php
@@ -23,7 +23,7 @@
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'marker' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'marker' ),
 				'after'  => '</div>',
 			) );
 		?>