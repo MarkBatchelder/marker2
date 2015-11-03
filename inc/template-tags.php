Automattic/_s

Add post title to `edit_post_link`
This improves the accessibility of these links, and gives more context for screen reader users. Now, they know exactly where the link goes.

Escaped output with `esc_html_x` and `wp_kses`.

Resolves: #774

inc/template-tags.php
@@ -123,7 +123,14 @@ function _s_entry_footer() {
 		echo '</span>';
 	}
 
-	edit_post_link( esc_html__( 'Edit', '_s' ), '<span class="edit-link">', '</span>' );
+	edit_post_link(
+		sprintf(
+			esc_html_x( 'Edit %s', 'name of current post', '_s' ),
+			wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+		),
+		'<span class="edit-link">',
+		'</span>'
+	);
 }
 endif;
 

template-parts/content-page.php
@@ -23,7 +23,16 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', '_s' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php
+			edit_post_link(
+				sprintf(
+					esc_html_x( 'Edit %s', 'name of current post', '_s' ),
+					wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
 

MarkBatchelder/marker2

inc/template-tags.php
@@ -36,8 +36,14 @@ function marker_posted_on() {
 
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 	
-	edit_post_link( __( 'Edit', 'marker' ), '<span class="edit-link">', '</span>' );
-
+	edit_post_link(
+		sprintf(
+			esc_html_x( 'Edit %s', 'name of current post', 'marker' ),
+			wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+		),
+		'<span class="edit-link">',
+		'</span>'
+	);
 }
 endif;
 
@@ -67,7 +73,14 @@ function marker_entry_footer() {
 		echo '</span>';
 	}
 
-	edit_post_link( esc_html__( 'Edit', 'marker' ), '<span class="edit-link">', '</span>' );
+	edit_post_link(
+		sprintf(
+			esc_html_x( 'Edit %s', 'name of current post', 'marker' ),
+			wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+		),
+		'<span class="edit-link">',
+		'</span>'
+	);
 }
 endif;
 

template-parts/content-page.php
@@ -29,7 +29,16 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', 'marker' ), '<span class="edit-link">', '</span>' ); ?>
+		<?php
+			edit_post_link(
+				sprintf(
+					esc_html_x( 'Edit %s', 'name of current post', 'marker' ),
+					wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
 