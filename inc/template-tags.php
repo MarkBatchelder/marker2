Automattic/_s

Add post title to `edit_post_link`
* Use `__()` instead of `_x()`
* Add translator comment, remove context modifier.
* Remove escaping on `the_title()`; it's trusted data.

Resolves: #774

inc/template-tags.php
@@ -125,8 +125,9 @@ function _s_entry_footer() {
 
 	edit_post_link(
 		sprintf(
-			esc_html_x( 'Edit %s', 'name of current post', '_s' ),
-			wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+			/* translators: %s: Name of current post */
+			esc_html__( 'Edit %s', '_s' ),
+			the_title( '<span class="screen-reader-text">"', '"</span>', false )
 		),
 		'<span class="edit-link">',
 		'</span>'

template-parts/content-page.php
@@ -26,8 +26,9 @@
 		<?php
 			edit_post_link(
 				sprintf(
-					esc_html_x( 'Edit %s', 'name of current post', '_s' ),
-					wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+					/* translators: %s: Name of current post */
+					esc_html__( 'Edit %s', '_s' ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
 				),
 				'<span class="edit-link">',
 				'</span>'

MarkBatchelder/marker2

inc/template-tags.php
@@ -75,8 +75,9 @@ function marker_entry_footer() {
 
 	edit_post_link(
 		sprintf(
-			esc_html_x( 'Edit %s', 'name of current post', 'marker' ),
-			wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+			/* translators: %s: Name of current post */
+			esc_html__( 'Edit %s', 'marker' ),
+			the_title( '<span class="screen-reader-text">"', '"</span>', false )
 		),
 		'<span class="edit-link">',
 		'</span>'

template-parts/content-page.php
@@ -32,8 +32,9 @@
 		<?php
 			edit_post_link(
 				sprintf(
-					esc_html_x( 'Edit %s', 'name of current post', 'marker' ),
-					wp_kses( the_title( '<span class="screen-reader-text">"', '"</span>', false ), array( 'span' => array( 'class' => array() ) ) )
+					/* translators: %s: Name of current post */
+					esc_html__( 'Edit %s', 'marker' ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
 				),
 				'<span class="edit-link">',
 				'</span>'