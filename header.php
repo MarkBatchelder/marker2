9e6980943525377b3612bcca70e532a22111c5d2
jrfnl <github_nospam@adviesenzo.nl>
2015-11-11 15:34:03 +0100

    Ignore WPCS output escape warning for description as it *is* already correctly escaped.
    Fixes failing travis build.

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 
 			<?php $description = get_bloginfo( 'description', 'display' );
 			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; ?></p>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
 		</div><!-- #logo -->
 
