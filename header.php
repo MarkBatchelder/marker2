4a8dc5453d97f6aae62821d1a4d342fe2418b201
Mark Batchelder <code@wpaddiction.com>
2015-11-15 23:02:03 -0800

    Changed button to hamburger menu.

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 		</div><!-- #logo -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'marker' ); ?></button>
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( '&#9776;', 'marker' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
