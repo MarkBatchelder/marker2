403c4ca2130f343bbb7455611bf66d25533c964b
byjml <byjml@users.noreply.github.com>
2015-12-06 15:24:01 +0300

    Use consistent syntax for control structures
    Merge pull request #882 from _s byjml/patch-1

--- template-parts/content.php
+++ template-parts/content.php
@@ -20,11 +20,11 @@
 
 	<header class="entry-header">
 		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-			}
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
