Automattic/_s

Avoid going in and out of PHP unnecessarily.
Closes #854

Includes minor alignment changes for easier readability/detection of start/end of conditions.

template-parts/content.php
@@ -11,13 +11,15 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 
-		<?php if ( 'post' === get_post_type() ) : ?>
+		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php _s_posted_on(); ?>
 		</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<?php
+		endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -27,9 +29,7 @@
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
-		?>
 
-		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
 				'after'  => '</div>',

MarkBatchelder/marker2

template-parts/content.php
@@ -17,13 +17,15 @@
 	<?php endif; ?>
 
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php
+            the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' );
 
-		<?php if ( 'post' === get_post_type() ) : ?>
+		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php marker_posted_on(); ?>
 		</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<?php
+		endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -33,9 +35,7 @@
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'marker' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
-		?>
-		
-		<?php
+
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'marker' ),
 				'after'  => '</div>',