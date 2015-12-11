a95b840189cc466e7367a01556a90e3e216ddcf7
Mark Batchelder <code@wpaddiction.com>
2015-12-11 02:53:23 -0800

    PHP error fixes, round 2.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php
     if ( is_front_page() ) :
-    	/* Don't display page title if front page is static. */
+        /* Don't display page title if front page is static. */
     else :
         <header class="entry-header">
             <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
