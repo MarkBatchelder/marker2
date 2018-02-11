0bd1b449fbd76f15c410ad1baaa277d5d31b59aa
WPAddiction <code@wpaddiction.com>
2018-02-10 16:49:31 -0800

    Slim down the travis script - remove superfluous checks.
    Fixes #881.
    
    Merge pull request #888 from _s jrfnl/feature/slim-down-travis

--- inc/customizer.php
+++ inc/customizer.php
@@ -21,6 +21,6 @@ add_action( 'customize_register', 'marker_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function marker_customize_preview_js() {
-	wp_enqueue_script( 'marker_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+	wp_enqueue_script( 'marker_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'marker_customize_preview_js' );
