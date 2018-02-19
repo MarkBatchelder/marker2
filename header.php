Automattic/_s

Fix skip to content link.
* Reverts this commit: https://github.com/Automattic/_s/pull/927 The issue for that, #928 referenced an old commit. The `id` is still in the source though.
* Brought up by #982. The `id` was correct, should not have been changed and this fixes everything.

Fixes #982

header.php
@@ -22,7 +22,7 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">

MarkBatchelder/marker2

header.php
@@ -23,7 +23,7 @@
 <body <?php body_class(); ?>>
 <div id="page-container" class="site-container">
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'marker' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'marker' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
 		<nav id="top-navigation" class="secondary-navigation" role="navigation">