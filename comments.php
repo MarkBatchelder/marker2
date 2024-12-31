e086e7dd478636cca287290017978689c737fedd
WPAddiction <code@wpaddiction.com>
2024-12-30 17:52:11 -0800

    Merge pull request #884 from dmtrmrv/comment-nav
    Introducing comment navigation template tags.

--- comments.php
+++ comments.php
@@ -45,17 +45,7 @@ if ( post_password_required() ) {
 			?>
 		</h2><!-- .comments-title -->
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-		<nav id="comment-nav-above" class="navigation comment-navigation">
-			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'marker' ); ?></h2>
-			<div class="nav-links">
-
-				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'marker' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'marker' ) ); ?></div>
-
-			</div><!-- .nav-links -->
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // Check for comment navigation. ?>
+        <?php the_comments_navigation(); ?>
 
 		<ol class="comment-list">
 			<?php
