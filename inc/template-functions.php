4687fc95e936136c9b192a5feaadbdbdf96a4f59
WPAddiction <code@wpaddiction.com>
2025-01-02 18:07:33 -0800

    Merge pull request #1173 from benoitchantre/no-sidebar-class
    no-sidebar class

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -17,6 +17,11 @@ function marker_body_classes( $classes ) {
         $classes[] = 'hfeed';
     }
 
+    // Adds a class of no-sidebar when there is no sidebar present.
+    if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+        $classes[] = 'no-sidebar';
+    }
+
     return $classes;
 }
 add_filter( 'body_class', 'marker_body_classes' );
