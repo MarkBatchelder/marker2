491db5661266c10afecf748634502d0da7f4fcd0
WPAddiction <code@wpaddiction.com>
2025-01-02 18:10:24 -0800

    Merge pull request #1255 from Automattic/add-neg-tabindex-to-featured-images
    Add 'tabindex=-1' to the featured image markup, to improve accessibil…

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -148,7 +148,7 @@ if ( ! function_exists( 'marker_post_thumbnail' ) ) :
 	
 		<?php else : ?>
 	
-		<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
+		<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
 			<?php
 			the_post_thumbnail( 'post-thumbnail', array(
 				'alt' => the_title_attribute( array(
